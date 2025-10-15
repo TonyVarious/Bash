d7bafa465a616f5f5540703e28f76b2990b7b71a
TonyVarious <55840003+TonyVarious@users.noreply.github.com>
2025-10-15 22:58:55 +0300

    Update tzdata.sh

--- tzdata.sh
+++ tzdata.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
+echo "Updating CentOS mirrors"
 sudo sed -i 's|mirrorlist=|#mirrorlist=|g' /etc/yum.repos.d/CentOS-*.repo
 sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*.repo
 
+echo "Updating tzdata"
 yum clean all
 yum update -y tzdata
